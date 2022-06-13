bcfa19c4d546b0465aa2651847a2be66b4a2bc35
Tuananh2222 <vut5441@gmail.com>
2022-06-13 23:05:31 +0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("btnCV").onclick = ()=>{
+ 
+  document.getElementById("btnCV").setAttribute("href","https://www.topcv.vn/xem-cv/UgAAXQUGBAdVBFIFDlNSVwdVBwFRAFQABFRRVQd18d")
+}
+
